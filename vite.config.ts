6a7ea7a8e747ca6171281fab2bfc9418a0a82b06
Adam Cleland <aclelandx@gmail.com>
2024-02-08 13:15:54 -0500

    updated config files

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgLoader from 'vite-svg-loader';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), svgLoader()],
-	base: '/Cleland-Utils',
+	base: './Cleland-Utils',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
