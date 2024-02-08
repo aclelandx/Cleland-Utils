f08f4bef1bec47407c158c1287ee2be2e2998e1d
Adam Cleland <aclelandx@gmail.com>
2024-02-08 13:33:32 -0500

    src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import '@assets/main.scss';
 import '@assets/mixins.scss';
-import App from './App.vue';
+import App from '@/App.vue';
 
 createApp(App).mount('#app');
