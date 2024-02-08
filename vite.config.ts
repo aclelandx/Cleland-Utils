5135b6caff1d947dbbef2bc7382c3e7dd7c1da87
Adam Cleland <aclelandx@gmail.com>
2024-02-08 11:22:05 -0500

    added base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
-import svgLoader from "vite-svg-loader";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), svgLoader()],
+	base: '/Cleland-Utils',
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "./src"),
-			"@components": path.resolve(__dirname, "./src/components"),
-			"@assets": path.resolve(__dirname, "./src/assets"),
-			"@types": path.resolve(__dirname, "./src/types"),
-			"@enums": path.resolve(__dirname, "./src/enums"),
-			"@utilities": path.resolve(__dirname, "./src/utilities"),
+			'@': path.resolve(__dirname, './src'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@types': path.resolve(__dirname, './src/types'),
+			'@enums': path.resolve(__dirname, './src/enums'),
+			'@utilities': path.resolve(__dirname, './src/utilities'),
 		},
 	},
 });
