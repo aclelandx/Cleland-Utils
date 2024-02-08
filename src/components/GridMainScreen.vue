0cb726065300b4112c1baa0a1365a7bd7d00ca52
Adam Cleland <aclelandx@gmail.com>
2024-02-08 13:21:55 -0500

    removed redundant import of define props

--- src/components/GridMainScreen.vue
+++ src/components/GridMainScreen.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, defineProps } from 'vue';
+import { computed } from 'vue';
 import ThemeSelector from '@components/ThemeSelector.vue';
 
 const props = defineProps<{
