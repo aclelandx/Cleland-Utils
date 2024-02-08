af84821fc974108ba20aa349ec85db478db3a628
Adam Cleland <aclelandx@gmail.com>
2024-02-08 17:14:52 -0500

    updated base path for vite to be repos location

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgLoader from 'vite-svg-loader';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), svgLoader()],
-	base: './',
+	base: '/Cleland-Utils/',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
